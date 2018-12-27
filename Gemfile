1df2bf15c8974ee801d8eff62e2a44e02ebbcc85
aronyu <h0935674938@hotmail.com>
2018-12-27 17:49:02 +0800

    feature(imgur) send image response

--- Gemfile
+++ Gemfile
@@ -5,4 +5,6 @@ gemspec
 group :development, :test do
   # ref: http://docs.rubocop.org/en/latest/installation/
   gem 'rubocop', '~> 0.56.0', require: false
+  gem 'faraday'
+  gem 'byebug'
 end
--- examples/echobot/app.rb
+++ examples/echobot/app.rb
@@ -1,11 +1,27 @@
 require 'sinatra'   # gem 'sinatra'
 require 'line/bot'  # gem 'line-bot-api'
+require 'faraday'
+require 'byebug'
 
 def client
   @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    config.channel_secret = '29a1930dc9b7dcf647626fd878fb48fc'
+    config.channel_token = 'OSZEquTZ8SEboALkvQiqCGFz85Vk2AajX4m+kf1L/R2eiDgfyyEgp51qkGtcpTXJhjSuCLUm4QAvYJpL7mjCe3247WYf86rdoyC00clouSf7bL4WtiLujJtw1rOngbF61Ah0RRO3PZ/QnyG6rwYSewdB04t89/1O/w1cDnyilFU='
   }
+end
+
+def get_image_url_form_imgur(word)
+  conn = Faraday.new(:url => 'https://api.imgur.com')
+  response = conn.get '/3/album/skSRl6l/images' do |req|
+    req.headers['Authorization'] = 'Client-ID 88377d50670b893'
+  end
+
+  data = JSON.parse(response.body)['data'].select { |d| d['description'] == word }
+  if data.any?
+    data.sample['link']
+  else
+    nil
+  end
 end
 
 post '/callback' do
@@ -23,11 +39,14 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
-        client.reply_message(event['replyToken'], message)
+        if image_url = get_image_url_form_imgur(event.message['text'])
+          message = {
+            type: 'image',
+            originalContentUrl: image_url,
+            previewImageUrl: image_url
+          }
+          client.reply_message(event['replyToken'], message)
+        end
       end
     end
   }
